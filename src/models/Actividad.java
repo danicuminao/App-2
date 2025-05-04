46d0f527508614ceb693d122642245ec71eb773b
danicuminao <dpiedad12@gmail.com>
2025-05-03 22:39:23 -0400

    archivo actividad.java

---
+++ src/models/Actividad.java
@@ -0,0 +1,27 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Actividad {
+    private String tipo;
+    private LocalDate fecha;
+    private boolean completada;
+
+    public Actividad(String tipo, LocalDate fecha) {
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.completada = false;
+    }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+    public LocalDate getFecha() { return fecha; }
+    public void setFecha(LocalDate fecha) { this.fecha = fecha; }
+    public boolean isCompletada() { return completada; }
+    public void setCompletada(boolean completada) { this.completada = completada; }
+
+    @Override
+    public String toString() {
+        return tipo + " (" + fecha + ")" + (completada ? " [COMPLETADA]" : "");
+    }
+}
